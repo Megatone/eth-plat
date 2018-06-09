45c9ca8f1cea8e8991d0d802c77e00d4c981277e
Megatone <inacio_pk2@hotmail.com>
2018-06-09 15:40:27 +0200

    a

--- server/index.js
+++ server/index.js
@@ -9,18 +9,26 @@ var socketsService = require('./app/sockets/service');
 
 //Connect to Mongo DB
 mongoose.Promise = global.Promise;
+const fs = require('fs')
+const https = require('https')
+const httpsOptions = {
+    cert: fs.readFileSync(path.join(__dirname, 'ssl', 'ca.crt')),
+    key: fs.readFileSync(path.join(__dirname, 'ssl', 'ca.key'))
+}
 
 
 
 mongoose.connect('mongodb://root:LZdyNO4NbG0jW97q@cluster0-shard-00-00-n9itl.mongodb.net:27017,cluster0-shard-00-01-n9itl.mongodb.net:27017,cluster0-shard-00-02-n9itl.mongodb.net:27017/ethereum_platform?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', { useMongoClient: true }).then(() => {
-    app.listen(8000, () => {
-//mongoose.connect('mongodb://' + settings.mongo.user + ':' + settings.mongo.password + '@' + settings.mongo.server + '/' + settings.mongo.database + '?retryWrites=true', { useMongoClient: true }).then(() => {
-    c.success('Application database connection with Mongo are be success');
-    c.info('MongoDb Server "' + settings.mongo.server + '"');
-    c.info('MongoDb Port "' + settings.mongo.port + '"');
-    c.info('MongoDb Database "' + settings.mongo.database + '"');
-    socketsService.initSocketService();
-    cronJobs.initJob();
+    // app.listen(8000, () => {
+
+    https.createServer(httpsOptions, app).listen(8000, () => {
+        //mongoose.connect('mongodb://' + settings.mongo.user + ':' + settings.mongo.password + '@' + settings.mongo.server + '/' + settings.mongo.database + '?retryWrites=true', { useMongoClient: true }).then(() => {
+        c.success('Application database connection with Mongo are be success');
+        c.info('MongoDb Server "' + settings.mongo.server + '"');
+        c.info('MongoDb Port "' + settings.mongo.port + '"');
+        c.info('MongoDb Database "' + settings.mongo.database + '"');
+        socketsService.initSocketService();
+        cronJobs.initJob();
     });
 }).catch(err => {
     c.danger(err)
